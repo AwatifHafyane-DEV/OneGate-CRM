6322395ec19adcecbab67c9a21a8997773313d84
abdoufk77 <ab.foukahy@gmail.com>
2025-05-05 21:53:16 +0100

    Initialisation du projet

---
+++ src/main/java/com/peacock/OneGate_CRM/OneGateCrmApplication.java
@@ -0,0 +1,13 @@
+package com.peacock.OneGate_CRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneGateCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneGateCrmApplication.class, args);
+	}
+
+}
